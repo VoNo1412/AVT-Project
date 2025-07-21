87c0e4ee1fb7466144076c619a69f18dc03d30d9
Vu Xuan Huy <xkaioken9x@gmail.com>
2025-07-21 21:26:29 +0700

    fix(PM-21): update port server be

--- server/src/server.js
+++ server/src/server.js
@@ -30,5 +30,5 @@ app.use((req, res) => {
 });
 
 // Khởi động server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
