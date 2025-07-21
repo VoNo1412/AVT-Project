a867fd9d3062e3338fa57be215bc57b4611a0875
Vu Xuan Huy <xkaioken9x@gmail.com>
2025-07-21 15:17:38 +0700

    fix(PM-21): setup deploy with docker hub

--- client/src/pages/LoginPages.jsx
+++ client/src/pages/LoginPages.jsx
@@ -85,7 +85,7 @@ function LoginPage() {
                 autoComplete="current-password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:outline-indigo-600 sm:text-sm"
                 placeholder="••••••••"
               />
             </div>
