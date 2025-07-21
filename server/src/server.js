63a223c8e3effbfeef5ee70784e71ffd043fe8a3
Vu Xuan Huy <xkaioken9x@gmail.com>
2025-07-21 23:04:09 +0700

    fix(PM-21): additional config header in nginx v7

--- server/src/server.js
+++ server/src/server.js
@@ -11,14 +11,15 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(helmet()); 
-app.use(cors({
-  origin: 'http://54.179.98.231',
-  credentials: true
-}));
+app.use(helmet()); // Bảo mật HTTP headers
+app.use(cors());
 app.use(express.json());
+
 // Kết nối MongoDB
-mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
