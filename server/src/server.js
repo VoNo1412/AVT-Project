bd72fca0069ee9583f88cd28566a43148efd83a7
Vu Xuan Huy <xkaioken9x@gmail.com>
2025-07-21 23:08:58 +0700

    fix(PM-21): additional config header in nginx v8

--- server/src/server.js
+++ server/src/server.js
@@ -16,10 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // Kết nối MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
